2e56ca66cb345d4a371d2ef8a2ffa39db2496f34
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-06-27 00:11:39 -0300

    feat: restructure API routes to group authentication-related endpoints

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+});
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
