68e745e1f85ce9d9e4cc978752b7b1a5ceb35f68
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-06-27 00:10:57 -0300

    test: enhance RegisterUserTest to validate response structure and duplicate email registration

--- backend/tests/Feature/RegisterUserTest.php
+++ backend/tests/Feature/RegisterUserTest.php
@@ -28,12 +28,27 @@ class RegisterUserTest extends TestCase
             $json->has('token');
             $json->has('type');
             $json->has('expires_in');
+            $json->has('message');
             $json->whereAllType([
                 'token' => 'string',
                 'type' => 'string',
-                'expires_in' => 'integer'
+                'expires_in' => 'integer',
+                'message' => 'string'
             ]);
         });
         $response->assertStatus(201);
+    }
+    public function test_regitration_user_should_fail_for_same_email(): void
+    {
+        $data = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->safeEmail,
+            'password' => 'P4assword!',
+            'password_confirmation' => 'P4assword!'
+        ];
+        $RegistrationResponse = $this->post('/api/auth/register', $data);
+        $RegistrationResponse->assertStatus(201); # Cria um usuário
+        $duplicatedRegisterResponse = $this->post('/api/auth/register', $data); # Tenta criar outro usuário com email igual
+        $duplicatedRegisterResponse->assertStatus(302)->assertSessionHasErrors('email');
     }
 }
