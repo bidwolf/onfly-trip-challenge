533bdb1cd6df2582f3b0f3b251811ed4b21f27c3
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-06-28 10:31:26 -0300

    feat: now api always expect that a request is expecting for a json response (formRequest json response needed)

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
@@ -17,6 +16,12 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->group('api', ['auth:api']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->shouldRenderJsonWhen(function (Request $request, \Throwable $e) {
+            if ($request->is('api/*')) {
+                return true;
+            }
+            return $request->expectsJson();
+        });
         $exceptions->render(function (AuthenticationException $e, Request $request) {
             if ($request->is('api/*')) {
                 return response()->json([
