4d7bbe29b717e567a0d05dbfdf8d8316c022bc1c
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-06-29 12:10:49 -0300

    feat: create TravelOrderFactory for generating travel order test data

---
+++ backend/database/factories/TravelOrderFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TravelOrder>
+ */
+class TravelOrderFactory extends Factory
+{
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id'        => User::factory(),
+            'requester_name' => fake()->name(),
+            'destination'    => fake()->city(),
+            'departure_date' => fake()
+                ->dateTimeBetween('now', '+1 year')
+                ->format('Y-m-d'),
+            'return_date' => fake()->dateTimeBetween('+1 year', '+2 years')->format('Y-m-d')
+        ];
+    }
+}
