5eb49328211da0da7b1d29ce1f5e3c14a14e80b9
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-06-30 02:20:33 -0300

    style: format array keys for consistency in TravelOrderResource

--- backend/app/Http/Resources/TravelOrderResource.php
+++ backend/app/Http/Resources/TravelOrderResource.php
@@ -15,14 +15,18 @@ class TravelOrderResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
+            'id'             => $this->id,
             'requester_name' => $this->requester_name,
-            'status' => $this->status,
-            'destination' => $this->destination,
+            'status'         => $this->status,
+            'destination'    => $this->destination,
             'departure_date' => $this->departure_date,
-            'return_date' => $this->return_date,
-            'user_id' => $this->user_id,
-            'created_at' => $this->created_at
+            'return_date'    => $this->return_date,
+            'user_id'        => $this->user_id,
+            'price'          => $this->price,
+            'hosting'        => $this->hosting,
+            'transportation' => $this->transportation,
+            'description'    => $this->description,
+            'created_at'     => $this->created_at,
         ];
     }
 }
