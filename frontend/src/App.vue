10b46d8a07a9254bfb86bbcced7134a99b418607
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-07-05 19:04:53 -0300

    feat: add Sonner styles to ensure toasts to be showed properly

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { Toaster } from '@/components/ui/sonner'
 import GlobalLoading from '@/components/GlobalLoading.vue'
+import 'vue-sonner/style.css'
+
 </script>
 
 <template>
