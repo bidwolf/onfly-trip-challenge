a1f9e548c73badce874e455297647a772dff0c37
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-07-05 18:47:16 -0300

    feat: add separate notifications that is going to be sent via broadcasts

---
+++ backend/app/Notifications/TravelOrderCancelled.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Enum\TravelOrderStatus;
+use App\Models\TravelOrder;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class TravelOrderCancelled extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(private TravelOrder $order)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['broadcast'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+    /**
+     * Determine if the notification should be sent.
+     */
+    public function shouldSend(object $notifiable, string $channel): bool
+    {
+        return $this->order->status === TravelOrderStatus::Cancelled;
+    }
+    /**
+     * Get the broadcastable representation of the notification.
+     */
+    public function toBroadcast(object $notifiable): BroadcastMessage
+    {
+        return new BroadcastMessage([
+            'order_id' => $this->order->id,
+            'amount' => $this->order->price,
+            'status' => $this->order->status->value
+        ]);
+    }
+}
